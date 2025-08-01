8a0a713619bf9e9830d2c0fed7c2c840e9368dcb
dzsigmond <dzsigmond@gmail.com>
2025-08-01 11:45:40 -0300

    Create extrair.js

---
+++ extrair.js
@@ -0,0 +1,10 @@
+// extrair.js
+export async function extrairDados(url) {
+  return {
+    url,
+    canal: 'Globo, Premiere', // valor simulado para teste
+    horario: '21:30',
+    mandante: 'Time A',
+    visitante: 'Time B'
+  };
+}
